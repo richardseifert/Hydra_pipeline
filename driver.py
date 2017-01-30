e3f8fb9e3cbb0bf945ba39f124acf36edf427e02
Richard Seifert <richardseifert@live.com>
2017-01-29 20:06:09 -0600

    Can now process flats to produce a master_flat and fiber_mask as intermediate products in calib/

--- driver.py
+++ driver.py
@@ -5,11 +5,42 @@
 
 import os
 import sys
-from process import process_flat
+import process
+from make_recipe import make_recipe
+
+
+#This function checks if a path to a directory exists. If the path does not
+#   exist, then it makes all of the necessary directories for the path to exist.
+#This function is used by order_spec objects when saving.
+def ensure_path(path):
+    if os.path.exists(path):
+        return path
+    slash_indxs = [indx for indx,char in enumerate(path) if char=="/"]
+    dirs_to_make = []
+    for indx in reversed(slash_indxs):
+        rmv = path[indx:].split("/")[1]
+        tmp_path = path[0:indx]+"/"
+        dirs_to_make.insert(0,tmp_path+rmv)
+        if os.path.exists(tmp_path):
+            found = True
+            break
+    if not found:
+        print "Path does not exist."
+        return path
+    print tmp_path, dirs_to_make[:-1]
+    for dirname in dirs_to_make[:-1]:
+        print dirname
+        os.makedirs(dirname)
+    return path
 
 if __name__ == '__main__':
     command = sys.argv[1]
     direc = sys.argv[2]
+    loc = 'indata/'+direc
+    
+    if command == 'make-recipe':
+        make_recipe(loc, 'recipes/'+direc+'.recipe')
 
-    if command = 'flat':
-        process_flat(direc)
+    if command == 'flat':
+        print 'call process_flat'
+        process.process_flat(direc)
---
+++ process.py
@@ -0,0 +1,36 @@
+from driver import ensure_path
+from fitstools import combine, save_2darr
+from astropy.io import fits
+from find_fibers import find_fibers
+
+def process_flat(dname, recipe=None):
+    if recipe == None:
+        recipe = 'recipes/'+dname+'.recipe'
+    
+    #Extract lines from recipe file that pertain to flat fields.
+    recipe_lines = [line for line in filter(None, open(recipe).read().split('\n')) if line[0] != '#']
+    flat_recipes = [line for line in recipe_lines if len(line.split(',')) > 2 and line.split(',')[1] == 'flat']
+
+    for r in flat_recipes:
+        flat_dorecipe(r, dname)
+
+def flat_dorecipe(r, dname):
+    info = r.split(',')
+    group_num = info[0]
+    print('Processing group', group_num, 'flats.')
+    rtype = info[1]
+    filenames = info[2].split(' ')
+    use_fibers = [int(f_num) for f_num in info[3].split(' ')]
+
+    #Make directory in calib for this group number
+    group_dir = 'calib/'+dname+'/group'+group_num+'/'
+    ensure_path(group_dir)
+
+    #Median combine flats, save to calib directory
+    flats = [fits.open('indata/'+dname+'/'+filename) for filename in filenames]
+    master_flat = combine(*flats)
+    master_flat.writeto(group_dir+'/master_flat.fits', clobber=True)
+
+    #Find fibers, make fiber mask, save to directory
+    fiber_mask = find_fibers(master_flat)
+    save_2darr(fiber_mask, group_dir+'fiber_mask.fits')
