2867ae7ca8ceeef35000fef3f8c30bdc78923676
Richard Seifert <richardseifert@live.com>
2017-03-21 22:04:36 -0500

    Now dividing out flat fiber profile in throughput map.

--- throughput.py
+++ throughput.py
@@ -12,11 +12,11 @@ def make_throughput_map(fmask, mflat):
     fnums = [int(n) for n in set(fmask.flatten()) if n != 0]
     
     #Correct for the profile of each flat fiber
-    #for fnum in fnums:
-    #    flat_spec = row_avg(mask_fits(mflat, fmask, maskval=fnum))
-    #    medianf = np.median(flat_spec)
-    #    for i,counts in enumerate(flat_spec):
-    #        throughput_map[i][np.where(fmask[i]==fnum)] *= flat_spec[i]/medianf
+    for fnum in fnums:
+        flat_spec = row_avg(mask_fits(mflat, fmask, maskval=fnum))
+        medianf = np.median(flat_spec)
+        for i,counts in enumerate(flat_spec):
+            throughput_map[i][np.where(fmask[i]==fnum)] *= flat_spec[i]/medianf
 
 
     #Correct for fiber-to-fiber throughput.
