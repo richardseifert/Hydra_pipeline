07b6756fbcc02a9e5dd46d1859e69abb54ba62d5
Richard Seifert <richardseifert@live.com>
2017-03-30 23:05:31 -0500

    Implemented spectrum object with can be added to and subtracted to with scipy.interpolate.interp1d

--- extract_spectra.py
+++ extract_spectra.py
@@ -4,6 +4,113 @@ plt.ion()
 from fitstools import mask_fits
 from scipy.interpolate import interp1d
 
+def get_x_interp(x_arrs, x_interp=None, x_interp_i=None, dx=None, **kwargs):
+    if x_interp == None:
+        try:
+            x_interp = x_arrs[x_interp_i]
+        except TypeError, IndexError:
+            low = max([min(x_arr) for x_arr in x_arrs]) #Find the lowest x value
+            high = min([max(x_arr) for x_arr in x_arrs]) #Find the highest x value
+            if dx != None:
+                x_interp = np.arange(low, high, dx)
+            else:
+                x_interp = []
+                num_x = len(x_arrs)
+                x_i_list = [0]*num_x
+                current_x = low
+                while current_x < high:
+                    x_interp.append(current_x)
+                    avg_dx = 0
+                    n = 0
+                    for i,x in enumerate(x_arrs):
+                        indx = x_i_list[i]
+                        while indx < len(x) and x[indx] < current_x:
+                            indx += 1
+                        x_i_list[i] = int(indx)
+                        try:
+                            avg_dx += abs(x[indx+1] - x[indx])
+                            n+=1
+                        except:
+                            pass
+                    avg_dx = avg_dx/n if n>0 else last_dx
+                    current_x += avg_dx
+                    last_dx = avg_dx
+    
+    return x_interp
+    
+def unpack_xy(use_args='all', preserve=False):
+    def decorator(f):
+        def wrapper(use_args, *args, **kwargs):
+            args = list(args)
+            if use_args == 'all':
+                use_args = [i for i in range(len(args))]
+            dtype = None
+            for i in use_args:
+                if isinstance(args[i], spectrum):
+                    x_arr, y_arr = args[i].get_data()
+                    dtype = lambda w, f: spectrum(w, f)
+                    d = 1
+                elif len(args[i]) == 2:
+                    x_arr, y_arr = args[i]
+                #Sort x and y
+                sort_i = np.argsort(x_arr)
+                y_arr = np.asarray(y_arr)[sort_i]
+                x_arr = np.asarray(x_arr)[sort_i]
+                args[i] = [x_arr, y_arr]
+            res = f(*args, **kwargs)
+            if preserve and dtype != None:
+                res = dtype(*res)
+            return res
+        return lambda *args, **kwargs: wrapper(use_args, *args, **kwargs)
+    return decorator
+
+class spectrum:
+    def __init__(self, wavelength, flux, header=None):
+        self.wav = wavelength
+        self.flux = flux
+        self.header = header
+    def set_header(self, new_header):
+        self.header = new_header
+    def get_wavelength(self):
+        return self.wav
+    def get_flux(self):
+        return self.flux
+    def get_data(self):
+        return [self.wav, self.flux]
+    @unpack_xy()
+    def math_helper(spec1, spec2, **kwargs):
+        s1_x, s1_y = spec1
+        s2_x, s2_y = spec2
+        x_interp = get_x_interp([s1_x, s2_x], **kwargs)
+        s1_y_interp = interp1d(s1_x, s1_y)(x_interp)
+        s2_y_interp = interp1d(s2_x, s2_y)(x_interp)
+        return x_interp, s1_y_interp, s2_y_interp
+
+    def __add__(self, other, **kwargs):
+        try:
+            x_interp, self_y_interp, other_y_interp = spectrum.math_helper(self, other, **kwargs)
+            y_interp = self_y_interp+other_y_interp
+            return spectrum(x_interp, y_interp)
+        except:
+            print 'BROKE'
+    def __sub__(self, other, **kwargs):
+        try:
+            x_interp, self_y_interp, other_y_interp = spectrum.math_helper(self, other, **kwargs)
+            y_interp = self_y_interp-other_y_interp
+            return spectrum(x_interp, y_interp)
+        except:
+            print 'BROKE'
+
+    def save(self, savepath):
+        np.savetxt(savepath, zip(*self.get_data()))
+    def plot(self, ax=None, **kwargs):
+        if ax == None:
+            fig, ax = plt.subplots()
+        ax.set_xlabel('Wavelength ($\AA$)')
+        ax.set_ylabel('Flux')
+        ax.plot(self.wav, self.flux, **kwargs)
+        return ax
+
 def extract(fiber_mask, fiber_num, img, wvlsol):
     '''
     Function that extracts a 1D spectrum for a specified fiber.
@@ -41,56 +148,40 @@ def extract(fiber_mask, fiber_num, img, wvlsol):
     #    flux += interp_flux
     wvlsol_slices = [fiber_wvlsol[:,i] for i in range(len(fiber_wvlsol[0]))]
     counts_slices = [fiber_counts[:,i] for i in range(len(fiber_counts[0]))]
-    wavelength, flux = interp_add(wvlsol_slices, counts_slices, x_interp_i=center_i)
+    wavelength, flux = interp_add(*zip(wvlsol_slices, counts_slices), x_interp_i=center_i)
     
-    return wavelength, flux
+    return spectrum(wavelength, flux)
 
+'''
+def unpack_spectra(use_args='all', preserve=True):
+    def decorator(f):
+        def wrapper(use_args, *args, **kwargs):
+            if use_args == 'all':
+                use_args = [i for i in range(len(args))]
+            for i in use_args:
+                if isinstance(args[i]
+'''
 
-def interp_add(x_arrs, y_arrs, x_interp=None, x_interp_i=None, dx=None):
-    if x_interp == None:
-        try:
-            x_interp = x_arrs[x_interp_i]
-        except TypeError, IndexError:
-            low = max([min(x_arr) for x_arr in x_arrs]) #Find the lowest x value
-            high = min([max(x_arr) for x_arr in x_arrs]) #Find the highest x value
-            if dx != None:
-                x_interp = np.arange(low, high, dx)
-            else:
-                #Sort the inputs
-                sort_i_list = [np.argsort(x_arr) for x_arr in x_arrs]
-                y_arrs = [np.asarray(y_arr)[sort_i] for y_arr, sort_i in zip(y_arrs, sort_i_list)]
-                x_arrs = [np.asarray(x_arr)[sort_i] for x_arr, sort_i in zip(x_arrs, sort_i_list)]
-                
-                x_interp = []
-                num_x = len(x_arrs)
-                x_i_list = [0]*num_x
-                current_x = low
-                while current_x < high:
-                    x_interp.append(current_x)
-                    avg_dx = 0
-                    n = 0
-                    for i,x in enumerate(x_arrs):
-                        indx = x_i_list[i]
-                        while indx < len(x) and x[indx] < current_x:
-                            indx += 1
-                        x_i_list[i] = int(indx)
-                        try:
-                            avg_dx += abs(x[indx+1] - x[indx])
-                            n+=1
-                        except:
-                            pass
-                    avg_dx /= n
-                    current_x += avg_dx
+@unpack_xy()
+def interp_helper(*xy_curves, **kwargs):
+    x_arrs = [curve[0] for curve in xy_curves]
+    y_arrs = [curve[1] for curve in xy_curves]
     
-    # x_interp should now be defined.
-    
-    x_interp = np.asarray(x_interp)
-    y_sum = np.zeros_like(x_interp)
-    for x_arr, y_arr in zip(x_arrs, y_arrs):
-        y_interp = interp1d(x_arr, y_arr)(x_interp)
-        y_sum += y_interp
+    x_interp = get_x_interp(x_arrs=x_arrs, **kwargs)
 
-    return x_interp, y_sum
+    y_interp_arrs = np.zeros((len(y_arrs), len(x_interp)))
+    for i in range(len(x_arrs)):
+        y_interp_arrs[i,:] = interp1d(x_arrs[i], y_arrs[i], fill_value=(np.nan, np.nan))(x_interp)
+    return x_interp, y_interp_arrs
 
+@unpack_xy(preserve=True)
+def interp_add(*spectra, **kwargs):
+    x_interp, y_interp_arrs = interp_helper(*spectra, **kwargs)
+    y_interp = np.nansum(y_interp_arrs, axis=0)
+    return x_interp, y_interp
 
-
+@unpack_xy(preserve=True)
+def interp_mean(*spectra, **kwargs):
+    x_interp, y_interp_arrs = interp_helper(*spectra, **kwargs)
+    y_interp = np.nanmean(y_interp_arrs, axis=0)
+    return x_interp, y_interp
