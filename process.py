bf5d8f77cf99ed5a60c3a04e5baa99bc4c5fe826
Richard Seifert <richardseifert@live.com>
2017-03-30 23:02:20 -0500

    Now prints status and keeps log of reducitions.

--- process.py
+++ process.py
@@ -7,10 +7,11 @@ from astropy.io import fits
 from find_fibers import find_fibers
 from throughput import make_throughput_map
 from find_wvlsol import wvlsol
-from extract_spectra import extract
+from extract_spectra import spectrum, extract, interp_mean
 from sys import stdout
 import os
 from os.path import exists
+import time
 
 class output_log:
     def __init__(self, writer=stdout, log_path=None):
@@ -20,9 +21,11 @@ class output_log:
         self.writer = writer
         self.progress_str = ""
         self.message_str = ""
+        self.coverlen = 0
     def update(self):
         strg = self.progress_str+' '+self.message_str
-        strg = strg.ljust(80)
+        strg = strg.ljust(self.coverlen)
+        self.coverlen = len(strg)
         self.writer.write('\r'+strg)
         self.writer.flush()
     def edit_progress(self, new_str):
@@ -32,8 +35,9 @@ class output_log:
         self.message_str = new_str
         self.update()
         if add_to_log and self.log_path != None:
+            dt_str = time.strftime("%Y-%m-%dT%H:%M:%S")
             f = open(self.log_path, 'a')
-            f.write(self.message_str+'\n')
+            f.write('['+dt_str+'] '+self.message_str+'\n')
     def linebreak():
         self.writer.write('\n')
 
@@ -50,18 +54,7 @@ def get_recipes(dname, recipe=None, gnum=None, rtype=None):
         recipe_lines = [line for line in recipe_lines if len(line.split(',')) > 1 and int(line.split(',')[0]) == gnum]
     return recipe_lines
 
-def get_num_groups(dname, recipe):
-    if recipe == None:
-        recipe = 'recipes/'+dname+'.recipe'
-    f = open(recipe)
-    recipe_lines = [line for line in filter(None, f.read().split('\n')) if line[0] != '#']
-    f.close()
-    group_nums = [int(line.split(',')[0]) for line in recipe_lines]
-    num_groups = max(group_nums)
-    return num_groups
-
 def make_master_bias(dname, recipe=None, output=stdout):
-    num_groups = get_num_groups(dname, recipe)
     bias_recipes = get_recipes(dname, recipe, rtype='zero')
 
     calib_dir = 'calib/'+dname
@@ -81,31 +74,9 @@ def make_master_bias(dname, recipe=None, output=stdout):
             master_bias.writeto(calib_dir+'/master_bias.fits', clobber=True)
             for f in biases:
                 f.close()
-    '''
-    #Load all other files make a bias-corrected version
-    master_bias_data = master_bias[0].data
-    for gnum in range(1, num_groups+1):
-        group_recipes = get_recipes(dname, recipe, gnum=gnum)
-        group_dir = calib_dir+'/group'+str(gnum)+'/'
-        ensure_path(group_dir)
-        fnames = []
-        for r in group_recipes:
-            fnames.extend(filter(None, r.split(',')[2].split(' ')))
-        num_images = len(fnames)
-        for i,fname in enumerate(fnames):
-            output.write('\rBias correcting: Group '+str(gnum)+'/'+str(num_groups)+', Image '+str(i+1)+'/'+str(num_images))
-            output.flush()
-            f = fits.open('indata/'+dname+'/'+fname)
-            f[0].data  = f[0].data - master_bias_data
-            f[0].header['COMMENT'] = 'This image is bias corrected.'
-            new_fname = fname.split('.')[0]+bc_suffix+'.fits'
-            f.writeto(group_dir+'/'+new_fname, clobber=True)
-            f.close()
-    output.write('\n')
-    '''
 
 def bias_correct(image, bias=None):
-    if bias == None:
+    if type(bias) == type(None):
         #Use bias overscan region of the image.
         #Figure out later.
         return image
@@ -125,11 +96,10 @@ def process_flat(dname, recipe=None, output=stdout):
         #output.write('\rProcessing '+dname+' flats: '+str(i+1)+'/'+str(num_r))
         #output.flush()
         flat_dorecipe(r, dname, recipe, output)
-    output.breakline()
 
 def flat_dorecipe(r, dname, recipe, output=None):
     #Unpack info from recipe line.
-    output.edit_message('Retrieving flat frames.')
+    output.edit_message('Reading recipe.')
     info = r.split(',')
     group_num = info[0]
     rtype = info[1]
@@ -139,7 +109,7 @@ def flat_dorecipe(r, dname, recipe, output=None):
     #Make directory in calib for this group number
     indata_dir = 'indata/'+dname
     calib_dir = 'calib/'+dname+'/group'+group_num
-    ensure_path(calib_dir)
+    ensure_path(calib_dir+'/')
 
     #Obtain master bias frame
     make_master_bias(dname, recipe)
@@ -152,9 +122,11 @@ def flat_dorecipe(r, dname, recipe, output=None):
         master_bias = None
 
     #Load flat frames, bias correct, and median combine flats, save to calib directory
-    output.edit_message('Bias correcting flat frames.')
+    output.edit_message('Loading flat frames.')
     flats = [fits.open(indata_dir+'/'+filename) for filename in filenames]
+    output.edit_message('Bias correcting flat frames.')
     flats = [bias_correct(flat, master_bias) for flat in flats]
+    output.edit_message('Median combining flat frames.')
     master_flat = combine(*flats)
     for flat in flats:
         flat.close()
@@ -175,33 +147,19 @@ def flat_dorecipe(r, dname, recipe, output=None):
     tm_path = calib_dir+'/throughput_map.fits'
     fits.writeto(tm_path, throughput_map, clobber=True)
     output.edit_message('Throughput map saved at '+tm_path)
-    
-    #Throughput correct all images from this pointing.
-    #recipe_lines = get_recipes(dname, recipe, gnum=group_num)
-    #for r in recipe_lines:
-    #    info = r.split(',')
-    #    recipe_filenames = info[2].split(' ')
-    #    filenames = [fname.split('.')[0]+bc_suffix+'.fits' for fname in recipe_filenames]
-    #    for fname in filenames:
-    #        #Load file
-    #        f = fits.open(group_dir+'/'+fname)
-    #        f[0].data  = f[0].data / throughput_map
-    #        f[0].header['COMMENT'] = 'This image is throughput corrected.'
-    #        new_fname = fname.split(bc_suffix)[0]+tc_suffix+'.fits'
-    #        f.writeto(group_dir+'/'+new_fname, clobber=True)
 
-
-def process_thar(dname, recipe=None, output=stdout):
+def process_thar(dname, recipe=None, output=stdout, **kwargs):
+    output = output_log(log_path='calib/'+dname+'/output.log')
     thar_recipes = get_recipes(dname, recipe, rtype='comp')
 
     num_r = len(thar_recipes)
     for i,r in enumerate(thar_recipes):
-        output.write('\rProcessing '+dname+' thar: '+str(i+1)+'/'+str(num_r))
-        output.flush()
-        thar_dorecipe(r, dname)
-    output.write('\n')
+        output.edit_progress('Processing '+dname+' thar: '+str(i+1)+'/'+str(num_r)+' |')
+        output.edit_message('')
+        thar_dorecipe(r, dname, output, **kwargs)
 
-def thar_dorecipe(r, dname):
+def thar_dorecipe(r, dname, output=None, **kwargs):
+    output.edit_message('Reading thar recipe.')
     #Unpack info from recipe line.
     info = r.split(',')
     group_num = info[0]
@@ -212,7 +170,7 @@ def thar_dorecipe(r, dname):
     #Make directory in calib for this group number
     indata_dir = 'indata/'+dname
     calib_dir = 'calib/'+dname+'/group'+group_num
-    ensure_path(calib_dir)
+    ensure_path(calib_dir+'/')
 
     #Obtain master bias frame
     mb_path = 'calib/'+dname+'/master_bias.fits'
@@ -232,23 +190,27 @@ def thar_dorecipe(r, dname):
     fm.close()
     
     for fname in filenames:
-       comp = fits.open(indata_dir+'/'+fname)
-       comp[0].data = (comp[0].data - master_bias) / throughput_map
-       wvlsol_map = wvlsol(comp, fiber_mask, use_fibers)
-       fits.writeto(calib_dir+'/wvlsol.fits', wvlsol_map, clobber=True)
+        output.edit_message('Finding wavelength solution from '+fname)
+        comp = fits.open(indata_dir+'/'+fname)
+        comp[0].data = (comp[0].data - master_bias) / throughput_map
+        wvlsol_map = wvlsol(comp, fiber_mask, use_fibers, **kwargs)
+        ws_path = calib_dir+'/wvlsol.fits'
+        fits.writeto(ws_path, wvlsol_map, clobber=True)
+        output.edit_message('Wavelength solution saved at '+ws_path)
 
 def process_sky(dname, recipe=None, output=stdout):
+    output = output_log(log_path='calib/'+dname+'/output.log')
     sky_recipes = get_recipes(dname, recipe, rtype='sky')
 
     num_r = len(sky_recipes)
     for i,r in enumerate(sky_recipes):
-        output.write('Processing '+dname+' sky: '+str(i+1)+'/'+str(num_r))
-        output.flush()
-        sky_dorecipe(r, dname)
-    output.write('\n')
+        output.edit_progress('Processing '+dname+' sky: '+str(i+1)+'/'+str(num_r)+' |')
+        output.edit_message('')
+        sky_dorecipe(r, dname, output)
 
-def sky_dorecipe(r, dname):
+def sky_dorecipe(r, dname, output=None):
     #Unpack info from the recipe line.
+    output.edit_message('Reading sky recipe.')
     info = r.split(',')
     group_num = info[0]
     rtype = info[1]
@@ -257,12 +219,13 @@ def sky_dorecipe(r, dname):
 
     #If there are no fibers to be reduced, stop.
     if len(use_fibers) == 0:
+        output.edit_message('No fibers to be reduced. Stopping.')
         return
 
     #Make directory in calib for this group number
     indata_dir = 'indata/'+dname
     calib_dir = 'calib/'+dname+'/group'+group_num
-    ensure_path(calib_dir)
+    ensure_path(calib_dir+'/')
 
     #Obtain master bias frame, if it exists.
     mb_path = 'calib/'+dname+'/master_bias.fits'
@@ -289,34 +252,47 @@ def sky_dorecipe(r, dname):
     ws.close()
 
     #Make master sky frame
+    output.edit_message('Loading sky frames.')
     skys = [fits.open(indata_dir+'/'+filename) for filename in filenames]
+    output.edit_message('Median combining sky frames.')
     master_sky = combine(*skys)
-    master_sky = bias_correct(master_sky, master_bias)
-    master_sky[0].data /= throughput_map
-    master_sky.writeto(calib_dir+'/master_sky.fits', clobber=True)
     for sky in skys:
         sky.close()
+    output.edit_message('Bias correcting master sky frame.')
+    master_sky = bias_correct(master_sky, master_bias)
+    output.edit_message('Throughput correcting master sky frame.')
+    master_sky[0].data /= throughput_map
+    ms_path = calib_dir+'/master_sky.fits'
+    master_sky.writeto(ms_path, clobber=True)
+    output.edit_message('Master sky frame saved at '+ms_path)
 
+    sky_specs = []
     for fnum in use_fibers:
-        wavelength, flux = extract(fiber_mask, fnum, master_sky, wvlsol_map)
-        if True:
-            fig, ax = plt.subplots()
-            ax.set_title(str(fnum))
-            ax.plot(wavelength, flux)
+        output.edit_message('Extracting sky spectrum from fiber '+str(fnum))
+        sky_spec = extract(fiber_mask, fnum, master_sky, wvlsol_map)
+        sky_specs.append(sky_spec)
+    output.edit_message('Producing master sky spectrum')
+    master_sky_spec = interp_mean(*sky_specs)
+    master_sky_spec.plot()
+
+    mss_path = calib_dir+'/master_sky_spec.dat'
+    master_sky_spec.save(mss_path)
+    output.edit_message('Master sky spectrum saved at '+mss_path)
 
 
 def process_target(dname, recipe=None, output=stdout):
+    output = output_log(log_path='calib/'+dname+'/output.log')
     tar_recipes = get_recipes(dname, recipe, rtype='object')
 
     num_r = len(tar_recipes)
     for i,r in enumerate(tar_recipes):
-        output.write('Processing '+dname+' targets: '+str(i+1)+'/'+str(num_r))
-        output.flush()
-        target_dorecipe(r, dname)
-    output.write('\n')
+        output.edit_progress('Processing '+dname+' targets: '+str(i+1)+'/'+str(num_r)+' |')
+        output.edit_message('')
+        target_dorecipe(r, dname, output)
 
-def target_dorecipe(r, dname):
+def target_dorecipe(r, dname, output=None):
     #Unpack info from the recipe line.
+    output.edit_message('Reading target recipe.')
     info = r.split(',')
     group_num = info[0]
     rtype = info[1]
@@ -325,6 +301,7 @@ def target_dorecipe(r, dname):
 
     #If there are no fibers to be reduced, stop.
     if len(use_fibers) == 0:
+        output.edit_message('No fibers to be reduced. Stopping.')
         return
 
     #Define paths to indata, calib, and outdata
@@ -358,22 +335,37 @@ def target_dorecipe(r, dname):
     throughput_map = tm[0].data
     tm.close()
 
+    #Load in master sky spectrum.
+    mss = np.loadtxt(calib_dir+'/master_sky_spec.dat')
+    master_sky_spec = spectrum(mss[:,0], mss[:,1])
+    master_sky_spec.plot()
+
     #Make master target frame
+    output.edit_message('Loading target frames.')
     tars = [fits.open(indata_dir+'/'+filename) for filename in filenames]
+    output.edit_message('Median combining target frames.')
     master_tar = combine(*tars)
-    master_tar = bias_correct(master_tar, master_bias)
-    master_tar[0].data /= throughput_map
-    master_tar.writeto(calib_dir+'/master_target_frame.fits', clobber=True)
     for f in tars:
         f.close()
+    output.edit_message('Bias correcting master target frame.')
+    master_tar = bias_correct(master_tar, master_bias)
+    output.edit_message('Throughput correcting master target frame.')
+    master_tar[0].data /= throughput_map
+    mt_path = calib_dir+'/master_target_frame.fits'
+    master_tar.writeto(mt_path, clobber=True)
+    output.edit_message('Master target frame saved at '+mt_path)
 
     header = master_tar[0].header
     for fnum in use_fibers:
+        output.edit_message('Extracting target spectrum from fiber '+str(fnum))
         tar_ID = filter(None, header['SLFIB'+str(fnum)].split(' '))[4]
-        wavelength, flux = extract(fiber_mask, fnum, master_tar, wvlsol_map)
-        np.savetxt(outdata_dir+'/'+tar_ID+'.txt', zip(wavelength, flux))
+        tar_spec = extract(fiber_mask, fnum, master_tar, wvlsol_map)
+        ts_path = outdata_dir+'/'+tar_ID+'.txt'
+        ax = tar_spec.plot(color='red')
+        tar_spec = tar_spec - master_sky_spec
+        tar_spec.plot(ax=ax)
+        tar_spec.save(ts_path)
+        output.edit_message('Target spectrum saved as '+ts_path)
 
         if True:
-            fig, ax = plt.subplots()
-            ax.set_title(tar_ID)
-            ax.plot(wavelength, flux)
+            tar_spec.plot()
