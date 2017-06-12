346e237de544797e982c5ee9c5398112a446fc7f
Richard Seifert <richardseifert@live.com>
2017-06-12 09:15:04 -0500

    Changed extraction method to Horne 1986aa method

--- libs/extract.py
+++ libs/extract.py
@@ -28,7 +28,7 @@ class fibers:
             header = fits.PrimaryHDU(np.array([])).header
         for i, fnum in enumerate(sorted(self.spectra.keys())):
             try:
-                apinfo = self.spectra[fnum].get_header()['SLFIB'+str(fnum)]
+                apinfo = self.spectra[fnum].get_header()['SLFIB'+str(int(fnum))]
             except TypeError:
                 apinfo = ''
             header['APINFO'+str(int(i+1))] = apinfo
@@ -45,7 +45,6 @@ class fibers:
         flux_err_dat = np.asarray([pad_array(spec.get_flux_err(), np.nan, length) for spec in self.get_spectra()])
         flux_err = fits.ImageHDU(flux_err_dat, header)
         flux_err.header['EXTNAME'] = 'FLUX_ERR'
-
 
         f = fits.HDUList([flux, wavelength, flux_err])
         f.writeto(savepath, clobber=True)
@@ -108,8 +107,11 @@ def simple_extraction(fiber_mask, fiber_num, img, wvlsol):
     return spectrum(wavelength, flux)
 
 @manage_dtype(with_header=[0])
-def optimal_extraction(image, fiber_mask, use_fibers, flat, wvlsol):
+def optimal_extraction(image, fiber_mask, profile_map, wvlsol=None, use_fibers=None):
+    if type(use_fibers)==type(None):
+        use_fibers = list({n for row in fiber_mask for n in row if n != 0})
     #Get image header and data
+    print image
     header = image[1]
     image = image[0]
 
@@ -135,34 +137,44 @@ def optimal_extraction(image, fiber_mask, use_fibers, flat, wvlsol):
         del h[kw]
 
     res = fibers()
-    for fnum in use_fibers:
-        image_fib = mask_fits(image, fiber_mask, maskval=fnum, reshape=True)
-        flat_fib = mask_fits(flat, fiber_mask, maskval=fnum, reshape=True)
-        wvlsol_fib = mask_fits(wvlsol, fiber_mask, maskval=fnum, reshape=True)
+    for fnum in use_fibers: 
+        print fnum
+
+        D = mask_fits(image, fiber_mask, maskval=fnum, reshape=True)
+
+        P = mask_fits(profile_map, fiber_mask, maskval=fnum, reshape=True)
+
         dn = mask_fits(dark_noise, fiber_mask, maskval=fnum, reshape=True)
+        one = np.ones_like(D)
+        V = (one*rn**2 + abs(gain*D) + dn**2)
+        
+        f_numer = P*D/V
+        var_numer = P/V
+        denom = P**2/V
 
-        one = np.ones_like(image_fib)
-        err = (one*rn**2 + abs(gain*image_fib) + dn**2)**0.5
-        weights = 1/err**2
+        if type(wvlsol)!=type(None):
+            wvlsol_fib = mask_fits(wvlsol, fiber_mask, maskval=fnum, reshape=True)
+            #Use the center of the fiber as the wavelength domain.
+            center_i = wvlsol_fib.shape[1]//2
+            wvlsol_slices = [wvlsol_fib[:,i] for i in range(len(wvlsol_fib[0]))]
+            f_numer_slices = [f_numer[:,i] for i in range(len(f_numer[0]))]
+            var_numer_slices = [var_numer[:,i] for i in range(len(var_numer[0]))]
+            denom_slices = [denom[:,i] for i in range(len(denom[0]))]
+            wavelength, sum_f_numer = interp_add(*zip(wvlsol_slices, f_numer_slices), x_interp_i=center_i)
+            wavelength, sum_var_numer = interp_add(*zip(wvlsol_slices, var_numer_slices), x_interp_i=center_i)
+            wavelength, sum_denom = interp_add(*zip(wvlsol_slices, denom_slices), x_interp_i=center_i)
+        else:
+            sum_f_numer = np.sum(f_numer, axis=0)
+            sum_var_numer = np.sum(var_numer, axis=0)
+            sum_denom = np.sum(denom, axis=0)
+            wavelength = list(range(len(sum_f_numer)))
 
-        wfi = weights*flat_fib*image_fib
-        wff = weights*flat_fib**2
-
-        #Use the center of the fiber as the wavelength domain.
-        center_i = wvlsol_fib.shape[1]//2
-        wvlsol_slices = [wvlsol_fib[:,i] for i in range(len(wvlsol_fib[0]))]
-        wfi_slices = [wfi[:,i] for i in range(len(wfi[0]))]
-        wff_slices = [wff[:,i] for i in range(len(wff[0]))]
-        wavelength, sum_wfi = interp_add(*zip(wvlsol_slices, wfi_slices), x_interp_i=center_i)
-        wavelength, sum_wff = interp_add(*zip(wvlsol_slices, wff_slices), x_interp_i=center_i)
-
-        flux = sum_wfi/sum_wff
-        flux_err = 1/sum_wff**0.5
+        f = sum_f_numer/sum_denom
+        var_f = sum_var_numer/sum_denom
 
         fiber_h = h.copy()
         fiber_h['SLFIB'+str(int(fnum))] = APINFO[int(fnum)]
-        spec = spectrum(wavelength, flux, flux_err, header=fiber_h)
+        spec = spectrum(wavelength, flux=f, flux_err=var_f**0.5, header=fiber_h)
         res.add_spectrum(fnum, spec)
-    res.save('/Users/richardseifert/Desktop/WIYN/Hydra_reductions/testing_new/saved_spectra/saved_fibers.fits')
 
     return res
