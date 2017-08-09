4ae57f4b54733591d45595cf5de4a94aaa242f06
Richard Seifert <richardseifert@live.com>
2017-08-09 12:15:35 -0500

    Improved the way recipe logs are generated.

--- libs/make_recipe.py
+++ libs/make_recipe.py
@@ -1,4 +1,5 @@
-from pointings import get_pointings
+from pointings import load_conditions, get_pointings
+import os
 
 class recipe(object):
 	def __init__(self, strg, delimiter=',', list_delimiter=' '):
@@ -17,15 +18,12 @@ def make_recipe(direc, savepath):
     recipe = open(savepath, 'w')
     recipe.write('#POINTING,IMAGE_TYPE,FILE_NAMES,FIBER_NUMBERS\n')
 
-
-
-    biases, pointings = get_pointings(direc)
-    #List bias info
-    file_names = [f.filename().split('/')[-1] for f in biases]
-    recipe.write('-1,zero,'+' '.join(file_names)+',\n')
-    for f in biases:
-        f.close()
-
+    if os.path.exists('recipes/recipe_config/recipe_conditions.txt'):
+        conditions = load_conditions('recipes/recipe_config/recipe_conditions.txt')
+        pointings = get_pointings(direc, conditions)
+    else:
+        pointings = get_pointings(direc)
+    
     for i,p in enumerate(pointings):
         pnum = str(i+1)
         if len(p.get_images()) == 0:
@@ -34,42 +32,60 @@ def make_recipe(direc, savepath):
 
         #List flat info
         img_type = 'flat'
-        file_names = [f.filename().split('/')[-1] for f in p.images[img_type]]
-        use_fiber = lambda slfib_s: len(slfib_s.split()) > 1 and slfib_s.split()[1] in ['0','1','3'] #0 is Sky, 1 is Object, 3 is Random
-        fiber_nums = get_use_fiber_nums(sample_header, use_fiber)
-        recipe.write(pnum+','+img_type+','+' '.join(file_names)+','+' '.join([str(n) for n in fiber_nums])+'\n')
+        if img_type in p.images:
+            file_names = [f.filename().split('/')[-1] for f in p.images[img_type]]
+            use_fiber = lambda slfib_s: len(slfib_s.split()) > 1 and slfib_s.split()[1] in ['0','1','3'] #0 is Sky, 1 is Object, 3 is Random
+            fiber_nums = get_use_fiber_nums(sample_header, use_fiber)
+            recipe.write(pnum+','+img_type+','+' '.join(file_names)+','+' '.join([str(n) for n in fiber_nums])+'\n')
 
         #List comp info
         img_type = 'comp'
-        file_names = [f.filename().split('/')[-1] for f in p.images[img_type]]
-        use_fiber = lambda slfib_s: len(slfib_s.split()) > 1 and slfib_s.split()[1] in ['0','1','3'] #0 is Sky, 1 is Object, 3 is Random
-        fiber_nums = get_use_fiber_nums(sample_header, use_fiber)
-        recipe.write(pnum+','+img_type+','+' '.join(file_names)+','+' '.join([str(n) for n in fiber_nums])+'\n')
+        if img_type in p.images:
+            file_names = [f.filename().split('/')[-1] for f in p.images[img_type]]
+            use_fiber = lambda slfib_s: len(slfib_s.split()) > 1 and slfib_s.split()[1] in ['0','1','3'] #0 is Sky, 1 is Object, 3 is Random
+            fiber_nums = get_use_fiber_nums(sample_header, use_fiber)
+            recipe.write(pnum+','+img_type+','+' '.join(file_names)+','+' '.join([str(n) for n in fiber_nums])+'\n')
 
         #List sky info
         img_type = 'sky'
-        file_names = [f.filename().split('/')[-1] for f in p.images['object']]
-        use_fiber = lambda slfib_s: len(slfib_s.split()) > 1 and slfib_s.split()[1] in ['0','3'] #0 is Sky, 3 is Random
-        fiber_nums = get_use_fiber_nums(sample_header, use_fiber)
-        recipe.write(pnum+','+img_type+','+' '.join(file_names)+','+' '.join([str(n) for n in fiber_nums])+'\n')
+        if 'object' in p.images:
+            file_names = [f.filename().split('/')[-1] for f in p.images['object']]
+            use_fiber = lambda slfib_s: len(slfib_s.split()) > 1 and slfib_s.split()[1] in ['0','3'] #0 is Sky, 3 is Random
+            fiber_nums = get_use_fiber_nums(sample_header, use_fiber)
+            recipe.write(pnum+','+img_type+','+' '.join(file_names)+','+' '.join([str(n) for n in fiber_nums])+'\n')
         
         #List object info
         img_type = 'object'
-        file_names = [f.filename().split('/')[-1] for f in p.images[img_type]]
-        use_fiber = lambda slfib_s: len(slfib_s.split()) > 1 and slfib_s.split()[1] in ['1'] #1 is Object
-        fiber_nums = get_use_fiber_nums(sample_header, use_fiber)
-        recipe.write(pnum+','+img_type+','+' '.join(file_names)+','+' '.join([str(n) for n in fiber_nums])+'\n')
+        if img_type in p.images:
+            file_names = [f.filename().split('/')[-1] for f in p.images[img_type]]
+            use_fiber = lambda slfib_s: len(slfib_s.split()) > 1 and slfib_s.split()[1] in ['1'] #1 is Object
+            fiber_nums = get_use_fiber_nums(sample_header, use_fiber)
+            recipe.write(pnum+','+img_type+','+' '.join(file_names)+','+' '.join([str(n) for n in fiber_nums])+'\n')
+            
+        #List skyflat info
+        img_type = 'skyflat'
+        if img_type in p.images:
+            file_names = [f.filename().split('/')[-1] for f in p.images[img_type]]
+            use_fiber = lambda slfib_s: len(slfib_s.split()) > 1 and slfib_s.split()[1] in ['0','3'] #0 is Sky, 3 is Random
+            fiber_nums = get_use_fiber_nums(sample_header, use_fiber)
+            recipe.write(pnum+','+img_type+','+' '.join(file_names)+','+' '.join([str(n) for n in fiber_nums])+'\n')
 
+        recipe.write('\n')
         p.close_files()
 
     recipe.close()
+
+fname = 'recipes/recipe_config/ignore_fibers.dat'
+f = open(fname)
+ignore_fibers = [int(n) for n in filter(None,f.read().split(','))]
+f.close()
 
 def get_use_fiber_nums(header, use_condition):
     use_fiber_nums = []
     n = 1
     k = 'SLFIB'+str(n)
     while k in header:
-        if use_condition(header[k]):
+        if use_condition(header[k]) and not n in ignore_fibers:
             use_fiber_nums.append(n)
         n += 1
         k = 'SLFIB'+str(n)
--- libs/pointings.py
+++ libs/pointings.py
@@ -2,14 +2,36 @@ import glob
 from datetime import datetime
 from astropy.io import fits
 
+default_conditions = {'object':lambda im: im[0].header['IMGTYPE']=='object',
+                      'flat':lambda im: im[0].header['IMGTYPE']=='flat',
+                      'comp':lambda im: im[0].header['IMGTYPE']=='comp'}
+
+def load_conditions(fname):
+    f = open(fname)
+    lines = filter(None, f.read().split('\n'))
+    f.close()
+
+    conditions = {}
+    for line in lines:
+        recipe = line.split(':')[0].lstrip(' ').rstrip(' ')
+        c = line.split(':')[1].replace('header[', 'im[0].header[')
+        exec('f = lambda im: '+c)
+        conditions[recipe] = f
+
+    return conditions
+
+
 class pointing:
-    def __init__(self, obj=[], flat=[], comp=[]):
-        self.images = {'object' :list(obj), 'flat':list(flat), 'comp':list(comp)}
-    def add_image(self, im, imgtype):
-        try:
-            self.images[imgtype].append(im)
-        except KeyError:
-            print 'Unrecognize d image type:',imgtype
+    def __init__(self, conditions=default_conditions):
+        self.images = {}
+        self.conditions = conditions
+    def add_image(self, im):
+        recipe = get_recipe(im, conditions=self.conditions)
+        if recipe == None:
+            return
+        if not recipe in self.images.keys():
+            self.images[recipe] = []
+        self.images[recipe].append(im)
     def get_images(self, img_type=None):
         if img_type == None:
             imgs = []
@@ -28,36 +50,38 @@ def convert_timestr(s):
     s = s.split('.')[0]+':'+micro_seconds
     return datetime.strptime(s, '%Y-%m-%dT%H:%M:%S:%f')
 
+def get_recipe(im, conditions=default_conditions):
+    for recipe in conditions:
+        if conditions[recipe](im):
+            return recipe
+    return None
+
 #group images with identical fiber pointings that were taken chronologically
-def get_pointings(direc):
+def get_pointings(direc, conditions=default_conditions):
     #Get list of fits files in direc
     if direc[-1] != '/':
         direc += '/'
-    fitsList = glob.glob(direc+'*.fits')+glob.glob(direc+'*.fit')
+    fnames = glob.glob(direc+'*.fits')+glob.glob(direc+'*.fit')
 
     #Load fits files
-    fitsList = [fits.open(fpath) for fpath in fitsList]
+    images = [fits.open(fpath) for fpath in fnames]
 
     #Sort files chronologically by DATE-OBS 
-    fitsList = sorted(fitsList, key=lambda f: convert_timestr(f[0].header['DATE-OBS']))
+    images = sorted(images, key=lambda im: convert_timestr(im[0].header['DATE-OBS']))
 
-    biases = []
-    pointings = []
+    pointings = [pointing(conditions=conditions)]
     prev_fiberConfig = ''
-    for f in fitsList:
-        h = f[0].header
+    for im in images:
+        h = im[0].header
         n = 1
         fiberConfig = ''
         while 'SLFIB'+str(n) in h:
             fiberConfig += h['SLFIB'+str(n)]
             n += 1
-        if fiberConfig != prev_fiberConfig:
-            pointings.append(pointing())
-        if h['IMGTYPE'] == 'zero':
-            biases.append(f)
-        else:
-            pointings[-1].add_image(f, h['IMGTYPE'])
-            prev_fiberConfig = fiberConfig
+        if fiberConfig != prev_fiberConfig and len(pointings[-1].get_images()) > 0:
+            pointings.append(pointing(conditions=conditions))
+        pointings[-1].add_image(im)
+        prev_fiberConfig = fiberConfig
 
-    return biases, pointings
+    return pointings
 
