b86e36531bd5bb7b3fa639e8e97cb47eed24994f
Richard Seifert <richardseifert@live.com>
2018-01-18 16:26:35 -0600

    Added unit tests for output.py

--- libs/tests.py
+++ libs/tests.py
@@ -30,5 +30,44 @@ class test_ensure_path(unittest.TestCase):
     def test_4(self):
         self.path_test("what/../if/../I/../go/../back/../and/../forth")
 
+from output import output_log
+from sys import stdout
+class test_output_log(unittest.TestCase):
+    def setUp(self):
+        self.obj = output_log()
+        self.log_path = "_test_log_file.txt"
+    def tearDown(self):
+        if os.path.exists(self.log_path):
+            os.remove(self.log_path)
+
+    #Test whether a new log file is created when calling set_log_path.
+    def test_set_log_path(self):
+        self.obj.set_log_path(self.log_path)
+        self.assertEqual(log_path, self.obj.log_path)
+        self.assertTrue(os.path.exists(log_path))
+
+    #Test if object message_str is changed when calling edit_message.
+    # Also test that nothing is saved to log when add_to_log=False.
+    def test_edit_message_1(self):
+        self.obj.set_log_path(self.log_path)
+        new_message = "Test Message 1\n"
+        self.obj.edit_message(new_message, add_to_log=False)
+        self.assertEqual(new_message, self.obj.message_str)
+        f = open(self.log_path)
+        log_text = f.read()
+        f.close()
+        self.assertEqual(log_text, "")
+
+    #Test if message is saved to log correctly.
+    def test_edit_message_2(self):
+        self.obj.set_log_path(self.log_path)
+        new_message = "Test Message 2\n"
+        self.obj.edit_message(new_message, add_to_log=True)
+        f = open(self.log_path)
+        log_text = f.read()
+        f.close()
+        self.assertTrue(new_message in log_text)
+        #Later, add tests for date string in log.
+
 if __name__ == "__main__":
     unittest.main()
